7c8f05bc948226257582dcd77a52dfae9fab989d
Chemgyu <chemgyu98@snu.ac.kr>
2023-11-04 08:45:34 +0900

    data download

---
+++ data/load_hmnist.sh
@@ -0,0 +1,17 @@
+# https://github.com/ratschlab/GP-VAE/blob/master/data/load_hmnist.sh
+DATA_DIR="data/hmnist"
+random_mechanism="mnar"
+
+mkdir -p ${DATA_DIR}
+
+if [ "$random_mechanism" == "mnar" ] ; then
+    wget https://www.dropbox.com/s/xzhelx89bzpkkvq/hmnist_mnar.npz?dl=1 -O ${DATA_DIR}/hmnist_${random_mechanism}.npz
+elif [ "$random_mechanism" == "spatial"] ; then
+    wget https://www.dropbox.com/s/jiix44usv7ibv1z/hmnist_spatial.npz?dl=1 -O ${DATA_DIR}/hmnist_${random_mechanism}.npz
+elif [ "$random_mechanism" == "random" ] ; then
+    wget https://www.dropbox.com/s/7s5y70f4idw9nei/hmnist_random.npz?dl=1 -O ${DATA_DIR}/hmnist_${random_mechanism}.npz
+elif [ "$random_mechanism" == "temporal_neg" ] ; then
+    wget https://www.dropbox.com/s/fnqi4rv9wtt2hqo/hmnist_temporal_neg.npz?dl=1 -O ${DATA_DIR}/hmnist_${random_mechanism}.npz
+elif [ "$random_mechanism" == "temporal_pos" ] ; then
+    wget https://www.dropbox.com/s/tae3rdm9ouaicfb/hmnist_temporal_pos.npz?dl=1 -O ${DATA_DIR}/hmnist_${random_mechanism}.npz
+fi
---
+++ data/load_physionet.sh
@@ -0,0 +1,5 @@
+# https://github.com/ratschlab/GP-VAE/blob/master/data/load_physionet.sh
+DATA_DIR="data/physionet"
+
+mkdir -p ${DATA_DIR}
+wget https://www.dropbox.com/s/651d86winb4cy9n/physionet.npz?dl=1 -O ${DATA_DIR}/physionet.npz
